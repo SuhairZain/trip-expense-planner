feb054f5d7017ecd6a6d549d75a7911b7ed6e8d7
Suhair Zain <suhairzain11@gmail.com>
2021-07-23 12:28:14 +0530

    Finish the algo

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,57 @@
-import { findMaximumPool, IClash, IPerson } from "..";
+import {
+  findMaximumPool,
+  getExcludedPeopleOnInclude,
+  IClash,
+  IPerson,
+} from "..";
 
 const people: IPerson[] = [
-  { id: "1", amount: 1000 },
-  { id: "2", amount: 500 },
-  { id: "3", amount: 400 },
-  { id: "4", amount: 800 },
-  { id: "5", amount: 600 },
-  { id: "6", amount: 900 },
+  { id: "0", amount: 1000 },
+  { id: "1", amount: 500 },
+  { id: "2", amount: 400 },
+  { id: "3", amount: 800 },
+  { id: "4", amount: 600 },
+  { id: "5", amount: 900 },
 ];
+
+describe("WHEN getExcludedPeopleOnInclude", () => {
+  for (const person of people) {
+    describe(`WHEN given clashes with everyone else (${person.id})`, () => {
+      it("SHOULD return a list with everyone else", () => {
+        const excludedPeople = getExcludedPeopleOnInclude(
+          people,
+          people.filter((p) => p !== person).map((p) => [person, p]),
+          person
+        );
+
+        expect(excludedPeople).toHaveLength(5);
+        expect(excludedPeople).not.toContain(person);
+      });
+    });
+  }
+
+  for (const person of people) {
+    describe(`WHEN given clashes with no one (${person.id})`, () => {
+      it("SHOULD return an empty list", () => {
+        const excludedPeople = getExcludedPeopleOnInclude(people, [], person);
+
+        expect(excludedPeople).toHaveLength(0);
+      });
+    });
+  }
+
+  describe("WHEN given a single person list", () => {
+    it("SHOULD return an empty list", () => {
+      const excludedPeople = getExcludedPeopleOnInclude(
+        [people[0]],
+        [],
+        people[0]
+      );
+
+      expect(excludedPeople).toHaveLength(0);
+    });
+  });
+});
 
 describe("WHEN findMaximumPool", () => {
   describe("WHEN given a list with no clashes", () => {
@@ -29,6 +73,26 @@ describe("WHEN findMaximumPool", () => {
       expect(findMaximumPool(people, clashes)).toEqual([
         people[0],
         people[3],
+        people[5],
+      ]);
+    });
+  });
+
+  describe("WHEN given a list with complicated clashes", () => {
+    it("SHOULD return the list with the max amount", () => {
+      const clashes: IClash[] = [
+        [people[0], people[1]],
+        [people[0], people[2]],
+        [people[0], people[3]],
+        [people[0], people[4]],
+        [people[0], people[5]],
+      ];
+
+      expect(findMaximumPool(people, clashes)).toEqual([
+        people[1],
+        people[2],
+        people[3],
+        people[4],
         people[5],
       ]);
     });
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,28 @@ export interface IPerson {
 
 export type IClash = [IPerson, IPerson];
 
+export const getExcludedPeopleOnInclude = (
+  people: IPerson[],
+  clashes: IClash[],
+  includedPerson: IPerson
+) => {
+  if (people.length === 1) {
+    return [];
+  }
+
+  const everyOneElse = people.filter((p) => p !== includedPerson);
+
+  if (clashes.length === 0) {
+    return [];
+  }
+
+  const clashesWithTheIncluded = clashes
+    .filter(([p1, p2]) => p1 === includedPerson || p2 === includedPerson)
+    .map(([p1, p2]) => (p1 === includedPerson ? p2 : p1));
+
+  return everyOneElse.filter((p) => clashesWithTheIncluded.includes(p));
+};
+
 export const findMaximumPool = (
   people: IPerson[],
   clashes: IClash[]
@@ -13,11 +35,58 @@ export const findMaximumPool = (
     return people;
   }
 
-  const pool: IPerson[] = [];
-
-  for (const [first, second] of clashes) {
-    pool.push(first.amount > second.amount ? first : second);
+  if (people.length === 0) {
+    return [];
   }
 
-  return pool;
+  const includedPerson = people[0];
+  const excludedPeople = getExcludedPeopleOnInclude(
+    people,
+    clashes,
+    includedPerson
+  );
+
+  if (excludedPeople.length === 0) {
+    return [includedPerson].concat(
+      findMaximumPool(
+        people.filter((p) => p !== includedPerson),
+        clashes
+      )
+    );
+  }
+
+  const excludedPeopleSum = excludedPeople.reduce(
+    (acc, curr) => acc + curr.amount,
+    0
+  );
+
+  const includedPersonWorthMore = includedPerson.amount >= excludedPeopleSum;
+
+  if (includedPersonWorthMore) {
+    const otherThanExcludedAndIncluded = people.filter(
+      (p) => !excludedPeople.includes(p) && p !== includedPerson
+    );
+    const clashesOtherThanExcludedAndIncluded = clashes.filter(
+      ([p1, p2]) =>
+        !excludedPeople.includes(p1) &&
+        !excludedPeople.includes(p2) &&
+        p1 !== includedPerson &&
+        p2 !== includedPerson
+    );
+
+    return [includedPerson].concat(
+      findMaximumPool(
+        otherThanExcludedAndIncluded,
+        clashesOtherThanExcludedAndIncluded
+      )
+    );
+  }
+
+  const clashesWithoutIncluded = clashes.filter(
+    ([p1, p2]) => p1 !== includedPerson && p2 !== includedPerson
+  );
+  return findMaximumPool(
+    people.filter((p) => p !== includedPerson),
+    clashesWithoutIncluded
+  );
 };
